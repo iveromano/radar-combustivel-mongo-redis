6956d7700bf5747b8599a3a483e94979442b964f
Ivonete Romano <ive_romano@hotmail.com>
2026-05-02 19:59:21 -0300

    Add configuration file for environment variables

---
+++ queries/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv('.env.local')
+load_dotenv()
+
+MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/?directConnection=true')
+MONGO_DB = os.getenv('MONGO_DB', 'radar_combustivel')
+REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
+REDIS_PORT = int(os.getenv('REDIS_PORT', '6379'))
+REDIS_DB = int(os.getenv('REDIS_DB', '0'))
+POLL_INTERVAL_SECONDS = float(os.getenv('POLL_INTERVAL_SECONDS', '2'))
+BATCH_SIZE = int(os.getenv('BATCH_SIZE', '500'))
