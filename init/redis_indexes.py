7beffca10a78a1161d32dbf51303271deb29f267
Ivonete Romano <ive_romano@hotmail.com>
2026-05-03 22:41:24 -0300

    Update environment variable loading and defaults

--- init/redis_indexes.py
+++ init/redis_indexes.py
@@ -7,11 +7,11 @@ from redis import Redis
 from redis.commands.search.field import GeoField, NumericField, TagField, TextField
 from redis.commands.search.index_definition import IndexDefinition, IndexType
 
-load_dotenv('.env.local')
-load_dotenv()
+load_dotenv('.env.local', override=True)
+load_dotenv(override=True)
 
-MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/?directConnection=true')
-REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
+MONGO_URI = os.getenv('MONGO_URI', 'mongodb://mongo:27017/?replicaSet=rs0&directConnection=true')
+REDIS_HOST = os.getenv('REDIS_HOST', 'redis')
 REDIS_PORT = int(os.getenv('REDIS_PORT', '6379'))
 DB_NAME = os.getenv('MONGO_DB', 'radar_combustivel')
 POSTOS = 'postos'
